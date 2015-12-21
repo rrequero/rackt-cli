590bf32a3cc795cfefd892e89150e52ec1e320c2
Ra <rareq1987@gmail.com>
2015-12-21 22:14:54 +0100

    add cli

--- webpack.build.js
+++ webpack.build.js
@@ -3,43 +3,46 @@ var webpack = require('webpack');
 var BASE_DIR = process.cwd();
 var COMPONENT_FILE = process.env.COMPONENT_FILE;
 var COMPONENT_NAME = process.env.COMPONENT_NAME;
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var plugins = [];
 
-plugins.push(new ExtractTextPlugin(COMPONENT_FILE+'.css', {
-            allChunks: true
+plugins.push(new ExtractTextPlugin(COMPONENT_FILE + '.css', {
+    allChunks: true
 }));
 
 function getPackageMain() {
-  return require(path.resolve(BASE_DIR, 'package.json')).main;
+    return require(path.resolve(BASE_DIR, 'package.json')).main;
 }
 
-if (process.env.MINIFY) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
-  COMPONENT_FILE += '.min';
+if(process.env.MINIFY) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+    COMPONENT_FILE += '.min';
 }
 
 module.exports = {
-  entry: path.resolve(BASE_DIR, getPackageMain()),
-  output: {
-    filename: path.resolve(BASE_DIR, 'dist/' + COMPONENT_FILE + '.js'),
-    library: COMPONENT_NAME,
-    libraryTarget: 'umd'
-  },
-  externals: {
-    'react': 'React',
-    'react/addons': 'React',
-    'react-dom': 'ReactDOM'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: path.resolve(process.env.RACKT_PATH, 'node_modules/babel-loader')
-      }
-    ]
-  },
-  plugins: plugins
+    entry: path.resolve(BASE_DIR, getPackageMain()),
+    output: {
+        filename: path.resolve(BASE_DIR, 'dist/' + COMPONENT_FILE + '.js'),
+        library: COMPONENT_NAME,
+        libraryTarget: 'umd'
+    },
+    externals: {
+        'react': 'React',
+        'react/addons': 'React',
+        'react-dom': 'ReactDOM'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: path.resolve(process.env.RACKT_PATH, 'node_modules/babel-loader')
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: ExtractTextPlugin.extract("style",path.resolve(process.env.RACKT_PATH,"node_modules/css-loader") + '!' + path.resolve(process.env.RACKT_PATH,"node_modules/sass-loader"))
+        }]
+    },
+    plugins: plugins
 };
